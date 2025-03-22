cc45e01611e2eea039bb142a9100117ecd8f3476
jcac5 <jcac5@alu.ua.es>
2025-03-22 11:43:53 +0000

    Teleop topic subido

---
+++ catkin_ws/build/get_pointclouds/CMakeFiles/get_pointclouds_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/workspace/catkin_ws/devel/lib/get_pointclouds/get_pointclouds_node"
+  "/workspace/catkin_ws/devel/lib/get_pointclouds/get_pointclouds_node.pdb"
+  "CMakeFiles/get_pointclouds_node.dir/src/node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/get_pointclouds_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/get_pointclouds/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspace/catkin_ws/src/get_pointclouds
+# Build directory: /workspace/catkin_ws/build/get_pointclouds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
